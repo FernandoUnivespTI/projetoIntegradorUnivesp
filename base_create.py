4dff5e2f38925bf71ee3e7d18491067f8d0d14c3
Fernando dos Santos <2002140@aluno.univesp.br>
2021-09-26 15:57:08 -0300

    primeiro commit

---
+++ base_create.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+banco = sqlite3.connect('database.sqlite')
+
+cursor = banco.cursor()
+
+cursor.execute("CREATE TABLE Crud (id integer not null primary key, name varchar (100), email varchar (100), phone varchar (100), birthdate varchar (100) )")
+
+banco.commit()
